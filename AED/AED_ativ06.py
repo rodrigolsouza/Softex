351db15ab5ca785494e4ba570730a9a5ff5eea83
rodrigolsouza <rodrigo.lopessouza@hotmail.com>
2022-06-21 23:53:54 -0300

    Modulo01_ParteAED_ativ01_e_ativ03

---
+++ AED/AED_ativ06.py
@@ -0,0 +1,7 @@
+'''Crie um tipo abstrato de dado (TAD) para manipular números complexos na linguagem Python.
+O método deve:
+
+- calcular três números complexos;
+- realizar todas as operações básicas;
+- e imprimir as propriedades real e img do números.'''
+
---
+++ AED/AED_ativ08.py
@@ -0,0 +1,3 @@
+'''Crie uma árvore para cada lista abaixo, adicione um valor nela e remova outro, mas, em pelo menos uma das árvores, a remoção deve ser de um nó com dois filhos.
+Lista1 = 45,20,30,60,81,97,100,7,8,4
+Lista2 = 15,6,18,3,7,16,20,4'''
