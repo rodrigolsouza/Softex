584eb249289858feed8a57652558f24b25c161d7
rodrigolsouza <rodrigo.lopessouza@hotmail.com>
2022-12-19 17:08:45 -0300

    inicio bloco Padrões de Projeto

---
+++ Padroes_Projeto/Padroes_modulo_2/modulo_2_ativ_03.js
@@ -0,0 +1,7 @@
+/*
+Crie um adaptador que permita que um objeto do tipo Pato seja usado como se fosse um objeto do tipo Galinha.
+
+Siga o exemplo apresentado no Hipertexto 4 e crie as classes AdaptadorPato e AdaptadorPatoDemo para demonstrar o uso da classe AdaptadorPato.
+
+Trabalhe esse código em seu IDE, suba ele para sua conta no GitHub e compartilhe o link desse projeto no campo ao lado para que outros desenvolvedores possam analisá-lo.
+*/
